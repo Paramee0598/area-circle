82db869518a6000b75a6122f2a1685835e882b80
TonG <TonG@TONGTUNPHUK>
2020-08-16 14:20:18 +0700

    Add project files.

---
+++ area circle/Source.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h> 
+#include<conio.h>
+
+const  float P = 3.14285714;
+float caba(float r)
+{
+	return P * r * r;
+}
+
+int main()
+{
+	float R;
+	R = 0;
+
+	printf("input circle radius  : ");
+	scanf("%f", &R);
+
+	float area;
+	area = 0;
+	area = caba(R);
+	printf("area circle = %.4f", area);
+
+
+	return 0;
+}
+
+
